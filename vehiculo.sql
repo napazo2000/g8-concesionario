d31de7b87607a132cdd1f89af5b6fde0b7489da3
alex982015 <62242546+alex982015@users.noreply.github.com>
2020-05-17 16:12:50 +0200

    Add files via upload

---
+++ vehiculo.sql
@@ -0,0 +1,23 @@
+USE `concesionario`;
+
+DROP TABLE IF EXISTS `vehiculo`;
+
+CREATE TABLE `vehiculo` (
+  `idVehiculo` int NOT NULL AUTO_INCREMENT,
+  `marca` varchar(100) DEFAULT NULL,
+  `modelo` varchar(20) DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL,
+  `tipoOferta` varchar(20) DEFAULT NULL,
+  `kilometros` int DEFAULT NULL,
+  `cilindrada` varchar(20) DEFAULT NULL,
+  `precio` double DEFAULT NULL,
+  `tipoCombustible` varchar(20) DEFAULT NULL,
+  `tipoCambio` varchar(20) DEFAULT NULL,
+  `anyoFabricacion` int DEFAULT NULL,
+  `precioSinIva` double DEFAULT NULL,
+  `unidades` int DEFAULT NULL,
+  `foto` longblob,
+  PRIMARY KEY (`idVehiculo`)
+);
+
+show tables;
