9b516fd283d6b4fc327584318dbaa3a279762f15
Ignacio fernandez contreras <62117413+napazo2000@users.noreply.github.com>
2020-05-30 14:18:25 +0200

    Documentación V1

--- prConcesionarioV4/src/Tabla/TablaGestorVehiculos.java
+++ prConcesionarioV4/src/Tabla/TablaGestorVehiculos.java
@@ -11,10 +11,10 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-/*En esta clase, lo que vamos a construir es la tabla correspondiente a la operación, la cual está conectada a la base de datos,
+/*En esta clase, lo que vamos a construir es la tabla correspondiente a la vehículo, la cual está conectada a la base de datos,
 con las mismas variables que tiene operación, por tanto, en esta clase, vamos a ir añadiendo cada columna con un orden específico,
 que estará en función de lo que se haya configurado previamente. Primero añadiremos cada columna con las varibles, y posteriormente
-haremos un bucle que recorrar toda la tabla, y en cada fila le asocie un valor que obtendremos de los getter de la clase operación.
+haremos un bucle que recorrar toda la tabla, y en cada fila le asocie un valor que obtendremos de los getter de la clase vehículo.
 En este caso al tener que añadir una foto, tenemos que hacer un try-catch, para capturar la excepción
 */
 
